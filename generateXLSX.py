3ed1158b1e1514e8faa26c7cfb98bd8cbacef236
miaotony <41962043+miaotony@users.noreply.github.com>
2019-11-14 01:04:45 +0800

    Update generateXLSX. 增加函数注释

--- generateXLSX.py
+++ generateXLSX.py
@@ -11,6 +11,14 @@ import os
 
 
 def create_xls(lessons, semester_year, semester, stuID):
+    """
+    生成`.xlsx`表格文件
+    :param lessons: {list} 课程列表
+    :param semester_year: {str} 学年
+    :param semester: {str} 学期
+    :param stuID: {str} 学号
+    :return: book {class Workbook} 表格
+    """
     book = Workbook()
     sheet = book.create_sheet('classTable', 0)
     alignment = Alignment(
@@ -28,38 +36,46 @@ def create_xls(lessons, semester_year, semester, stuID):
     sheet.row_dimensions[2].height = 20
     # print('1\n')
     for i in range(1, 12):
-        sheet.cell(i+2, 1, i).alignment = alignment
-        sheet.row_dimensions[i+2].height = 60
+        sheet.cell(i + 2, 1, i).alignment = alignment
+        sheet.row_dimensions[i + 2].height = 60
     weekday = ['一', '二', '三', '四', '五', '六', '天']
     ab = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I']
     # print('2\n')
     for i in range(0, 7):
-        sheet.cell(2, i+2, '星期' + weekday[i]).alignment = alignment
-        sheet.column_dimensions[ab[i+1]].width = 20
+        sheet.cell(2, i + 2, '星期' + weekday[i]).alignment = alignment
+        sheet.column_dimensions[ab[i + 1]].width = 20
     # print('xls created\n')
     for lesson in lessons:
         unit = int(lesson.course_unit[0])
         day = int(lesson.day_of_week)
-        rag = ab[day]+str(unit+2)+':'+ab[day] + \
-            str(unit+1+len(lesson.course_unit))
+        rag = ab[day] + str(unit + 2) + ':' + ab[day] + \
+              str(unit + 1 + len(lesson.course_unit))
         # print(rag)
         sheet.merge_cells(rag)
-        if(sheet.cell(unit+2, day+1).value != None):
-            sheet.cell(unit+2, day+1).value = str(sheet.cell(unit+2,
-                                                             day+1).value) + '\n\n' + lesson.str_for_print
+        if sheet.cell(unit + 2, day + 1).value:  # Exist other lesson(s)
+            sheet.cell(unit + 2, day + 1).value = str(sheet.cell(unit + 2,
+                                                                 day + 1).value) + '\n\n' + lesson.str_for_print
         else:
-            sheet.cell(unit+2, day+1).value = lesson.str_for_print
-            sheet.cell(unit+2, day+1).alignment = alignment
-            sheet.cell(unit+2, day+1).fill = PatternFill('solid', 'FFFF00')
+            sheet.cell(unit + 2, day + 1).value = lesson.str_for_print
+            sheet.cell(unit + 2, day + 1).alignment = alignment
+            sheet.cell(unit + 2, day + 1).fill = PatternFill('solid', 'FFFF00')
             # stl = Side('double')
             # sheet.cell(unit+2, day+1).border = Border(stl,stl,stl,stl)
     return book
 
 
 def export_xls(xls, semester_year, semester, stuID):
+    """
+    导出表格文件
+    :param xls: {class Workbook} 表格
+    :param semester_year: {str} 学年
+    :param semester: {str} 学期
+    :param stuID: {str} 学号
+    :return: None
+    """
     try:
         filename = 'NUAAiCal-Data/NUAA-curriculum-' + \
-            semester_year + '-' + semester + '-' + stuID + '.xlsx'
+                   semester_year + '-' + semester + '-' + stuID + '.xlsx'
         xls.save(os.path.abspath(filename))
         print('表格文件已导出到\"' + os.path.abspath(filename) + '\"。')
     except Exception as e:
